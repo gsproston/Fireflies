e3a29a94f477b91d295994787ea2fbd496d8d21b
George Sproston <georges@bytesnap.co.uk>
2020-06-17 22:16:49 +0100

    Fireflies now glow

--- Fireflies/src/Fireflies.cpp
+++ Fireflies/src/Fireflies.cpp
@@ -4,6 +4,9 @@
 
 int main()
 {
+	sf::Clock clock;
+	sf::Time elapsedTime = clock.restart();
+
 	const int fireflyRadius = 10;
 	const int numFireflies = 30;
 
@@ -16,11 +19,38 @@ int main()
 	{
 		tFirefly()
 		{
-			freq = 1.0f;
-			rad = (rand() % 101) / 10.0f;
+			freq = rand() % 2 + 0.5;
+			// rad between 0 and fireflyRadius
+			rad = (rand() % (fireflyRadius * 10 + 1)) / 10.f;
+			glowing = rand() % 2;
+		}
+
+		void Tick(sf::Time elapsedTime)
+		{
+			// calculate amount of movement
+			const float diff = elapsedTime.asSeconds() * freq * fireflyRadius;
+			if (glowing)
+			{
+				rad += diff;
+				if (rad > fireflyRadius)
+				{
+					rad = rad - (fireflyRadius - rad) * 2;
+					glowing = false;
+				}
+			}
+			else
+			{
+				rad -= diff;
+				if (rad < 0)
+				{
+					rad = -rad;
+					glowing = true;
+				}
+			}
 		}
 
 		float freq;
+		bool glowing;
 		float rad;
 	};
 
@@ -52,7 +82,7 @@ int main()
 			}
 		}
 
-		window.display();
+		window.clear();
 
 		// draw the array of fireflies
 		for (int x = 0; x < aFireflies.size(); ++x)
@@ -63,6 +93,18 @@ int main()
 				firefly.setFillColor(sf::Color::Yellow);
 				firefly.setPosition(x * fireflyRadius * 2, y * fireflyRadius * 2);
 				window.draw(firefly);
+			}
+		}
+
+		window.display();
+		elapsedTime = clock.restart();
+
+		// tick the array of fireflies
+		for (int x = 0; x < aFireflies.size(); ++x)
+		{
+			for (int y = 0; y < aFireflies[x].size(); ++y)
+			{
+				aFireflies[x][y].Tick(elapsedTime);
 			}
 		}
 	}
